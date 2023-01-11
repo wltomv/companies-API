e88f078b88e7da1b97396f8eccc660b8cd036ec3
Walter Mach <owaldovelasz@gmail.com>
2023-01-11 14:59:30 -0600

    Add project files.

---
+++ companies-API/Controllers/CompaniesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace companies_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompaniesController : ControllerBase
+    {
+        // GET: api/<CompanyController>
+        [HttpGet]
+        public async Task<ActionResult<List<string>>> getCompanies()
+        {
+            //TODO: temporal data 
+            List<string> companies = new List<string>
+            {
+                "Company 1",
+                "Company 2",
+                "Company 3"
+            };
+            return Ok(companies);
+        }
+    }
+}
